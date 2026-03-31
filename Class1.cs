5d79afce89ffc1742076141e61414c4439810708
d11416146 <d11416146@gs.takming.edu.tw>
2026-03-31 14:07:08 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public struct student
+{
+	private int ID;
+	private string Name;
+	private Test Score;
+	pulic 
+
+	public student(int id, string n,int m, int f)
+	{
+		ID = id;
+		Name = n;
+		Score =new Test(m,f);
+
+		Count++;
+	}
+	public string Getstudent()
+	{
+		string str;
+		str = "學生編號" + ID.ToString();
+		str+= "\n學生姓名:" + Name;
+		str += "\n期中考成績:" + Score.GetMid().ToString();
+        str += "\n期末考成績:" + Score.GetFinal().ToString();
+        str += "\n平均成績:" + Score.GetAvg().ToString();
+		return str;
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace class_0331_1
+{
+    internal class Test
+    {
+        static void Main(string[] args)
+        {
+            student joe,marry;
+            joe = new student(1234, "AAA", 92, 88);
+            Console.WriteLine(joe.Getstudent());
+
+            Console.WriteLine(Student.count.ToString());
+
+            marry = new student(1234, "AAA", 92, 88);
+            Console.WriteLine(student.count.ToString());
+        }
+    }
+}
